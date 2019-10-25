e7a7b6687c02fe8e181bb6166c0cf3e18d6a1b52
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-10-24 23:11:40 -0700

    added formatting

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
-	res.sendFile('/public/simulations.html', {"root": __dirname});
+	res.sendFile('/public/simulations/simulations.html', {"root": __dirname});
 })
 
 app.get('/simulations', function(req, res) {
-	res.sendFile('/public/simulations.html', {"root": __dirname});
+	res.sendFile('/public/simulations/simulations.html', {"root": __dirname});
 })
-
 
 app.get('/constvel1', function(req, res) {
 	res.sendFile('/public/constvel1/constvel1.html', {"root": __dirname});
