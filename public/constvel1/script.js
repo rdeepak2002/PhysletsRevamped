b700c3b9c327012777708c6b73b6ccc864a9e835
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-10-25 11:09:47 -0700

    added grid

--- public/constvel1/script.js
+++ public/constvel1/script.js
@@ -1,26 +1,32 @@
+var canvas = document.getElementById('canvas1');
+
 var initialTime = 0;
-var scale = 100;
+var scale = 50;
 
 var initX = -3*scale;
 var initY = 0*scale;
 var x = initX;
 var y = initY;
 
-var radius = 1*scale;
+var radius = 0.5*scale;
 var canvasWidth = 15*scale;
 var canvasHeight = 10*scale;
 var maxX = canvasWidth - 1*scale - canvasWidth/2;
 
+var canvasPos = getPosition(canvas);
+var mouseX = -100;
+var mouseY = -100;
+
 function init() {
+  canvas.width  = canvasWidth;
+  canvas.height = canvasHeight;
+  canvas.addEventListener("mousemove", setMousePosition, false);
   initialTime = Date.now();
   window.requestAnimationFrame(draw);
 }
 
 function draw() {
   var ctx = document.getElementById('canvas1').getContext('2d');
-  var canvas = document.getElementById('canvas1');
-  canvas.width  = canvasWidth;
-  canvas.height = canvasHeight;
 
   ctx.globalCompositeOperation = 'destination-over';
   ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
@@ -28,6 +34,12 @@ function draw() {
   var curTime = Date.now();
 
   var dt = (curTime - initialTime)/1000;            // get delta time in seconds
+
+  // drawing at mouse position
+  ctx.font = "30px Comic Sans MS";
+  ctx.fillStyle = "red";
+  ctx.textAlign = "center";
+  ctx.fillText("(" + (mouseX/scale-5) + ", " + (mouseY/scale-2) + ")", mouseX, mouseY);     // fix this coordinate
 
   // drawing circle
   ctx.beginPath();
@@ -74,5 +86,25 @@ function convertCartesianY(y, height) {
   console.log(height/2 + y)
   return height/2 + y;
 }
+
+function setMousePosition(e) {
+  mouseX = e.clientX - canvasPos.x;
+  mouseY = e.clientY - canvasPos.y;
+}
+
+function getPosition(el) {
+  var xPosition = 0;
+  var yPosition = 0;
+ 
+  while (el) {
+    xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+    yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
+    el = el.offsetParent;
+  }
+  return {
+    x: xPosition,
+    y: yPosition
+  };
+}    
 
 init();
