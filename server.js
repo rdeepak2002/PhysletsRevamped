bef7596f7d0017e2c65327c0db48a3e55374abee
rdeepak2002 <rdeepak2002@gmail.com>
2019-10-25 20:09:22 -0700

    index only shows click here redirect after 3 seconds

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-	res.sendFile('/public/simulations/simulations.html', {"root": __dirname});
-})
-
 app.listen(port, function() {
 	console.log('app running')
 })
