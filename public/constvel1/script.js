8ce7caec7146e5b90951eead7e961c191332b106
rdeepak2002 <rdeepak2002@gmail.com>
2019-10-25 21:08:38 -0700

    added p5 canvas

--- public/constvel1/script.js
+++ public/constvel1/script.js
@@ -9,7 +9,7 @@ $(function(){
 function setup() {
   let canvas = createCanvas(window.innerWidth, window.innerHeight*.7);
   canvas.parent('canvas-parent');
-  let info = createDiv('Find the acceleration of the object.');
+  let info = createDiv('Find the velocity of the object.');
 }
 
 function draw() {
