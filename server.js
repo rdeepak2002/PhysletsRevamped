73ce885b461a91ac78abb56b516ce564d98b6da7
kirtan-shah <kirtanushah@gmail.com>
2019-10-25 19:22:15 -0700

    Touched up server stuff.

--- server.js
+++ server.js
@@ -3,18 +3,10 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
 	res.sendFile('/public/simulations/simulations.html', {"root": __dirname});
-})
-
-app.get('/simulations', function(req, res) {
-	res.sendFile('/public/simulations/simulations.html', {"root": __dirname});
-})
-
-app.get('/constvel1', function(req, res) {
-	res.sendFile('/public/constvel1/constvel1.html', {"root": __dirname});
 })
 
 app.listen(port, function() {
