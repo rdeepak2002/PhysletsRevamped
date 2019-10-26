1d2f99527e1cfe571ad58ad68f88c122c1916a3c
kirtan-shah <kirtanushah@gmail.com>
2019-10-25 19:36:05 -0700

    Const accel simulation updates.

---
+++ public/constaccel1d/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    let canvas = createCanvas(window.innerWidth, window.innerHeight*.7);
+    canvas.parent('canvas-parent');
+    let info = createDiv('Find the acceleration of the object.');
+}
+
+function draw() {
+    background(230);
+}
