3c40c1b325fcf5a22a781668d7225f309032e1fb
kirtan-shah <kirtanushah@gmail.com>
2019-10-26 17:08:25 -0700

    Update grid

--- public/constaccel1d/sketch.js
+++ public/constaccel1d/sketch.js
@@ -8,7 +8,9 @@ function setup() {
     let canvas = createCanvas(window.innerWidth, window.innerHeight*.7)
     canvas.parent('canvas-parent')
     let info = createDiv('Find the acceleration of the object.')
-    grid = new Grid(-2, 10, -2, 2)
+    let ratio = height / width
+    let dim = ratio * 14 / 2
+    grid = new Grid(-4, 10, -dim, dim)
 }
 
 function draw() {
@@ -16,6 +18,8 @@ function draw() {
 
     let {x, y} = grid.project(mouseX, mouseY)
     text(`(${x.toFixed(2)}, ${y.toFixed(2)})`, mouseX, mouseY);
+
+    grid.draw(1, 1)
 }
 
 class Grid {
@@ -27,13 +31,24 @@ class Grid {
     }
 
     project(screenX, screenY) {
-        let x = screenX / width * (this.xmax - this.xmin) + this.xmin;
-        let y = (height - screenY) / height * (this.ymax - this.ymin) + this.ymin;
+        let x = screenX / width * (this.xmax - this.xmin) + this.xmin
+        let y = (height - screenY) / height * (this.ymax - this.ymin) + this.ymin
         return {x, y}
     }
     unproject(gridX, gridY) {
-        let x = (gridX - xmin) / (xmax - xmin) * width;
-        let y = height - ((gridX - ymin) / (ymax - ymin) * height);
+        let x = (gridX - this.xmin) / (this.xmax - this.xmin) * width
+        let y = height - ((gridY - this.ymin) / (this.ymax - this.ymin) * height)
         return {x, y}
+    }
+
+    draw(scaleX, scaleY) {
+        for(let x = this.xmin; x < this.xmax; x += scaleX) {
+            let xDraw = this.unproject(x, 0).x
+            line(xDraw, 0, xDraw, height)
+        }
+        for(let y = this.ymin; y < this.ymax; y += scaleY) {
+            let yDraw = this.unproject(0, y).y
+            line(0, yDraw, width, yDraw)
+        }
     }
 }
