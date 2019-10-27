02df205f5433b9f3288e69daae1594b01d16c15c
kirtan-shah <kirtanushah@gmail.com>
2019-10-26 17:01:07 -0700

    Updated accel1d

--- public/constaccel1d/sketch.js
+++ public/constaccel1d/sketch.js
@@ -1,13 +1,39 @@
 $(function(){
-  $("#nav-placeholder").load("/navbar/navbar.html");
-});
+    $("#nav-placeholder").load("/navbar/navbar.html")
+})
+
+let grid
 
 function setup() {
-    let canvas = createCanvas(window.innerWidth, window.innerHeight*.7);
-    canvas.parent('canvas-parent');
-    let info = createDiv('Find the acceleration of the object.');
+    let canvas = createCanvas(window.innerWidth, window.innerHeight*.7)
+    canvas.parent('canvas-parent')
+    let info = createDiv('Find the acceleration of the object.')
+    grid = new Grid(-2, 10, -2, 2)
 }
 
 function draw() {
-    background(230);
+    clear()
+
+    let {x, y} = grid.project(mouseX, mouseY)
+    text(`(${x.toFixed(2)}, ${y.toFixed(2)})`, mouseX, mouseY);
+}
+
+class Grid {
+    constructor(xmin, xmax, ymin, ymax) {
+        this.xmin = xmin
+        this.xmax = xmax
+        this.ymin = ymin
+        this.ymax = ymax
+    }
+
+    project(screenX, screenY) {
+        let x = screenX / width * (this.xmax - this.xmin) + this.xmin;
+        let y = (height - screenY) / height * (this.ymax - this.ymin) + this.ymin;
+        return {x, y}
+    }
+    unproject(gridX, gridY) {
+        let x = (gridX - xmin) / (xmax - xmin) * width;
+        let y = height - ((gridX - ymin) / (ymax - ymin) * height);
+        return {x, y}
+    }
 }
